952f2eedf093d4e37c32ee3f2d293dd3cffba2c6
Patryk 'pcpl2' Ławicki <kontakt@patryk-lawicki.pl>
2024-10-23 14:44:58 +0200

    Added basic resize file.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
-use actix_multipart::form::MultipartForm;
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
-use futures::future;
+use std::io::Cursor;
+
 use actix_files as fs;
+use actix_multipart::form::MultipartForm;
 use actix_web::http::header::{ContentDisposition, DispositionType};
-use actix_web::{ Error, HttpRequest};
+use actix_web::middleware::Logger;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::{Error, HttpRequest};
+use base64::prelude::BASE64_STANDARD;
+use base64::Engine;
+use futures::future;
+use image::ImageReader;
 
 mod models;
 
 #[post("/v1/newImage")]
-async fn post_new_image(form: web::Form<models::rest_models::ImagePayload>) -> HttpResponse {
-    HttpResponse::Ok().body(format!("username: {}", form.id))
+async fn post_new_image(form: web::Json<models::rest_models::ImagePayload>) -> HttpResponse {
+    let imageDecoded = BASE64_STANDARD.decode(form.image.clone());
+    if imageDecoded.is_err() {
+        return HttpResponse::BadRequest().body("Cannot decode image");
+    }
+    let img2 = ImageReader::new(Cursor::new(imageDecoded.unwrap()))
+        .with_guessed_format().unwrap()
+        .decode();
+    let img3 = img2.unwrap().resize(100, 100, image::imageops::FilterType::Gaussian);
+    img3.save_with_format("converted.avif", image::ImageFormat::Avif);
+    HttpResponse::Ok().body(format!("username: {}", form.image))
 }
 
 #[post("/v1/newImageMP")]
-pub async fn post_image_multipart(MultipartForm(form): MultipartForm<models::rest_models::ImageMultipartPayload>) -> impl Responder {
+pub async fn post_image_multipart(
+    MultipartForm(form): MultipartForm<models::rest_models::ImageMultipartPayload>,
+) -> impl Responder {
     format!(
         "Uploaded file {}",
         form.files.first().unwrap().file_name.as_ref().unwrap()
@@ -32,22 +49,20 @@ async fn images(req: HttpRequest) -> Result<fs::NamedFile, Error> {
         }))
 }
 
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let admin_api = HttpServer::new(|| {
         App::new()
+            .wrap(Logger::default())
             .service(post_image_multipart)
             .service(post_new_image)
     })
     .bind(("127.0.0.1", 9324))?
     .run();
 
-    let public_api = HttpServer::new(|| {
-        App::new().service(images)
-    })
-    .bind(("127.0.0.1", 9555))?
-    .run();
+    let public_api = HttpServer::new(|| App::new().service(images))
+        .bind(("127.0.0.1", 9555))?
+        .run();
     future::try_join(admin_api, public_api).await?;
 
     Ok(())
